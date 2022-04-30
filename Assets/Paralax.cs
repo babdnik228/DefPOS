3463fa1fdefe5debd10c3e525212cf79e52d3675
alexgodi <103599099+babdnik228@users.noreply.github.com>
2022-04-30 19:12:42 +0300

    Beta 1.0

---
+++ Assets/Paralax.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    [SerializeField]
+    private float _speedParalax;
+    [SerializeField]
+    private float startX = 0;
+    [SerializeField]
+    private float endX;
+    private void Update()
+    {
+        
+    }
+}
